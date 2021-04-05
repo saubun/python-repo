e08e3ada6c7a4ba9692f2b714bdb0055562aea49
saubun <dameon0430@gmail.com>
2021-04-05 11:43:38 -0400

    balls

--- src/balls.py
+++ src/balls.py
@@ -1,6 +1,7 @@
 import pygame
+import math
 
-WIDTH = 360
+WIDTH = 600
 HEIGHT = 480
 FPS = 60
 
@@ -12,18 +13,67 @@ BLUE = (0, 0, 255)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("<Your game>")
+pygame.display.set_caption("Balls")
 clock = pygame.time.Clock()
 
-running = True
-while running:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    screen.fill(BLACK)
+class Ball(object):
+    def __init__(self, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
 
-    pygame.display.update()
+    def draw(self):
+        self.pos = pygame.Vector2(self.x, self.y)
+        pygame.draw.circle(screen, WHITE, self.pos, self.r)
 
-pygame.quit()
+
+def main():
+    running = True
+    balls = []
+    radius = 0
+    tempball = Ball(0, 0, 0)
+    while running:
+        clock.tick(FPS)
+        if pygame.mouse.get_pressed()[0]:
+            radius += 1
+
+            pos = pygame.mouse.get_pos()
+            tempball = Ball(pos[0], pos[1], radius)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                del tempball
+                pos = pygame.mouse.get_pos()
+                balls.append(Ball(pos[0], pos[1], radius))
+                radius = 0
+
+        screen.fill(BLACK)
+
+        for ball in balls:
+            ball.draw()
+
+            # Collision detection for each ball
+            for nball in balls:
+                if nball != ball:
+                    dx = ball.x - nball.x
+                    dy = ball.y - nball.y
+                    distance = math.sqrt(dx * dx + dy * dy)
+
+                    if distance < ball.r + nball.r:
+                        print('collision')
+
+        try:
+            tempball.draw()
+        except Exception:
+            pass
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
