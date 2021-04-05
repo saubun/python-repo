e45d43776e88c44334e294c9888de7054b113152
saubun <dameon0430@gmail.com>
2021-04-05 11:08:55 -0400

    helloworld

---
+++ src/balls.py
@@ -0,0 +1,29 @@
+import pygame
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("<Your game>")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BLACK)
+
+    pygame.display.update()
+
+pygame.quit()
