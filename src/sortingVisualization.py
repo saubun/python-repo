d7000baf7509c6a1a16011b0eff3637da2ccdaeb
saubun <dameon0430@gmail.com>
2021-04-06 19:38:38 -0400

    sorting visualizer :)

--- src/sortingVisualization.py
+++ src/sortingVisualization.py
@@ -1,8 +1,9 @@
 import pygame
+import numpy as np
 
 WIDTH = 600
 HEIGHT = 480
-FPS = 60
+FPS = 10
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -15,13 +16,17 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sorting")
 clock = pygame.time.Clock()
 
-arr = [5, 7, 4, 7, 5, 4, 3, 2, 6, 5, 4, 1]
+# Setup an array of length 135 with numbers from 10 - 400
+arr = [np.random.randint(10, 400) for _ in range(135)]
+newArr = []
 
 
 def main():
+    '''The main function containing the game loop'''
     running = True
-    i = 20
     while running:
+
+        # Limit FPS
         clock.tick(FPS)
 
         # Main event listening
@@ -32,15 +37,41 @@ def main():
         # Reset display
         screen.fill(BLACK)
 
-        for item in arr:
-            i += 40
-            p = pygame.Rect(i, HEIGHT - 20, 20, 0)
-            p.inflate_ip(0, item * 20)
+        # Reset dummy value for spacing
+        dummy = 30
+
+        # Sorting
+        if len(arr) > 0:
+            min = 999  # Very large default min value not in the arr
+            for item in arr:
+                if item < min:
+                    min = item
+            newArr.append(min)
+            try:
+                arr.remove(min)
+            except ValueError:
+                pass
+
+            # Draw the array
+            for item in arr:
+                dummy += 4
+                p = pygame.Rect(WIDTH - dummy, HEIGHT - 20, 2, 0)
+                p.inflate_ip(0, item)
+                p.bottom = HEIGHT - 20
+                pygame.draw.rect(screen, WHITE, p)
+
+        # Reset dummy value for spacing
+        dummy = 30
+
+        # Draw new array
+        for item in newArr:
+            dummy += 4
+            p = pygame.Rect(dummy, HEIGHT - 20, 2, 0)
+            p.inflate_ip(0, item)
             p.bottom = HEIGHT - 20
             pygame.draw.rect(screen, WHITE, p)
 
-        i = 20
-
+        # Update display
         pygame.display.update()
 
     pygame.quit()
