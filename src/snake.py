152550139d782668400e0e15c03606c6a6008aa9
saubun <dameon0430@gmail.com>
2021-04-06 13:25:48 -0400

    Finished snake

--- src/snake.py
+++ src/snake.py
@@ -1,16 +1,23 @@
 import pygame
+import pygame.freetype
 import random
-import math
 
 WIDTH = 600
 HEIGHT = 480
-FPS = 10
+FPS = 8
+
+SIZE = 20
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+UP = 1
+DOWN = 2
+LEFT = 3
+RIGHT = 4
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -20,26 +27,30 @@ clock = pygame.time.Clock()
 Vector2 = pygame.Vector2
 
 
-def randomBase10Int(max, min=0):
-    '''Gets a random integer that is divisible by 10'''
+def randomBaseSIZEInt(max, min=0):
+    '''Gets a random integer that is divisible by SIZE'''
     num = random.randint(min, max)
-    num = math.ceil(num / 10) * 10
+    num = num // SIZE * SIZE
     return num
 
 
 class Snake:
     def __init__(self):
         '''The snake object'''
-        self.parts = [Vector2(300, 240)]
-        self.dx = 10
+        self.parts = [Vector2(WIDTH//2, HEIGHT//2)]
+        self.dx = SIZE
         self.dy = 0
         self.length = len(self.parts)
+        self.alive = True
+        self.dir = RIGHT
 
     def draw(self):
         '''Draws the snake on the screen part by part'''
         self.length = len(self.parts)
+        self.checkSelfCollision()
+        self.checkWallCollision()
         for part in self.parts:
-            partRect = pygame.Rect(part.x, part.y, 10, 10)
+            partRect = pygame.Rect(part.x, part.y, SIZE, SIZE)
             pygame.draw.rect(screen, GREEN, partRect, 1)
 
     def move(self):
@@ -51,73 +62,116 @@ class Snake:
 
     def checkWallCollision(self):
         '''Checks to see if the snake head is in contact with the wall'''
-        ...
+        if self.parts[0].x < 0:
+            self.alive = False
+        elif self.parts[0].x > WIDTH:
+            self.alive = False
+        elif self.parts[0].y < 0:
+            self.alive = False
+        elif self.parts[0].y > HEIGHT:
+            self.alive = False
 
     def checkSelfCollision(self):
-        '''Checks to see if the snake head is in contact with itself'''
-        ...
+        '''Checks to see if the snake head is in contact with itself,
+        this shouldn't be possible'''
+        for part in self.parts[1:]:
+            if self.parts[0] == part:
+                self.alive = False
 
 
 class Food:
     def __init__(self):
         '''Food object'''
-        self.pos = Vector2(randomBase10Int(WIDTH), randomBase10Int(HEIGHT))
+        p = Vector2(randomBaseSIZEInt(WIDTH-10), randomBaseSIZEInt(HEIGHT-10))
+        self.pos = p
 
     def draw(self):
         '''Draws the food onto the screen'''
-        foodRect = pygame.Rect(self.pos.x, self.pos.y, 10, 10)
-        pygame.draw.rect(screen, GREEN, foodRect)
+        self.rect = pygame.Rect(self.pos.x, self.pos.y, SIZE, SIZE)
+        pygame.draw.rect(screen, GREEN, self.rect)
 
 
 def main():
     running = True
     snake = Snake()
     foodCount = 0
+    global FPS
     while running:
+        # Limit frames
         clock.tick(FPS)
 
-        # Main event listening
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+        # Display score
+        pygame.display.set_caption(f"Snake - Score: {snake.length - 1}")
+
+        # Main Game
+        if snake.alive:
+
+            # Reset display
+            screen.fill(BLACK)
+
+            # Main event listening
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     running = False
-                if event.key == pygame.K_UP:
-                    snake.dx = 0
-                    snake.dy = -10
-                if event.key == pygame.K_DOWN:
-                    snake.dx = 0
-                    snake.dy = 10
-                if event.key == pygame.K_LEFT:
-                    snake.dx = -10
-                    snake.dy = 0
-                if event.key == pygame.K_RIGHT:
-                    snake.dx = 10
-                    snake.dy = 0
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP and snake.dir != DOWN:
+                        snake.dx = 0
+                        snake.dy = -SIZE
+                        snake.dir = UP
+                    if event.key == pygame.K_DOWN and snake.dir != UP:
+                        snake.dx = 0
+                        snake.dy = SIZE
+                        snake.dir = DOWN
+                    if event.key == pygame.K_LEFT and snake.dir != RIGHT:
+                        snake.dx = -SIZE
+                        snake.dy = 0
+                        snake.dir = LEFT
+                    if event.key == pygame.K_RIGHT and snake.dir != LEFT:
+                        snake.dx = SIZE
+                        snake.dy = 0
+                        snake.dir = RIGHT
 
-        # Reset display
-        screen.fill(BLACK)
+            # Create a new food once one is eaten
+            if foodCount == 0:
+                food = Food()
+                foodCount += 1
 
-        # Snake drawing
-        snake.draw()
-        snake.move()
+            # Snake drawing
+            snake.draw()
+            snake.move()
 
-        # Create a new food once one is eaten
-        if foodCount == 0:
-            food = Food()
-            foodCount += 1
+            # Draw the food
+            try:
+                food.draw()
+            except Exception:
+                pass
 
-        # Draw the food
-        try:
-            food.draw()
-        except Exception:
-            pass
+            # Food Collision
+            if snake.parts[0] == food.pos:
+                foodCount = 0
+                snake.parts.append(Vector2(-SIZE, -SIZE))
 
-        # Food Collision checkSelfCollision
-        if snake.parts[0] == food.pos:
-            foodCount = 0
-            snake.parts.append(snake.parts[len(snake.parts)-1])
+        # Game over
+        else:
+
+            # Reset display
+            screen.fill((50, 50, 50))
+
+            # Reset food
+            if foodCount != 0:
+                foodCount = 0
+
+            # Listen for restart & quiting
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                    if event.key == pygame.K_SPACE:
+                        snake.alive = True
+                        snake.parts = [snake.parts[0]]
+                        snake.parts[0] = Vector2(WIDTH//2, HEIGHT//2)
 
         pygame.display.update()
 
